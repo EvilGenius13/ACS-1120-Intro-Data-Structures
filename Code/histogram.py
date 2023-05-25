aae53c43db89b6a5fd96d2995f64e63403f25142
Josh Faigan <djjoker13@gmail.com>
2023-05-25 14:32:42 -0400

    added histogram

---
+++ Code/histogram.py
@@ -0,0 +1,39 @@
+import string
+from collections import Counter
+import timeit
+
+def create_word_histogram(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+
+        # Remove punctuation and numbers
+        translator = str.maketrans('', '', string.punctuation + string.digits)
+        text = text.translate(translator)
+
+        # Convert text to lowercase and split into words
+        words = text.lower().split()
+
+        # Create a word histogram
+        word_histogram = Counter(words)
+        # Backup method
+        # word_histogram = {}
+        # for word in words:
+        #     if word in word_histogram:
+        #         word_histogram[word] += 1
+        #     else:
+        #         word_histogram[word] = 1
+        return word_histogram
+
+# Txt file option
+file_path = 'data/paragraph.txt'
+
+# Measure execution time using timeit (lambda is an anonymous function)
+execution_time = timeit.timeit(lambda: create_word_histogram(file_path), number=1)
+
+# Print the histogram
+histogram = create_word_histogram(file_path)
+for word, count in histogram.items():
+    print(f'{word}: {count}')
+
+# Print the execution time
+print(f"Execution Time: {execution_time} seconds")
